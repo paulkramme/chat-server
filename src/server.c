1ad91d650019a65b426b3457258b05b6b992f8de
Paul Kramme <pjkramme@gmail.com>
2016-12-03 16:05:51 +0100

    Add logfile

--- src/server.c
+++ src/server.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
+#define PERMS 0666
+
+
 int main(int argc, char **argv)
 {
 	puts("Main chat server");
-	
+	puts("Server socket should start here...");
+	logfile = open("chatserver.log", O_WRONLY | O_CREAT | O_APPEND, PERMS)
 	return 0;
 }
