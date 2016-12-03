8e32ecf40403eda7f6f6796856a592eac3c37d2b
Paul Kramme <pjkramme@gmail.com>
2016-12-03 16:11:34 +0100

    Add log entry

--- src/server.c
+++ src/server.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv)
 	puts("Main chat server");
 	puts("Server socket should start here...");
 	logfile = open("chatserver.log", O_WRONLY | O_CREAT | O_APPEND, PERMS);
+	write(logfile, "server startup\n", sizeof("server startup\n"));	
+
 
 	close(logfile);
 	return 0;
